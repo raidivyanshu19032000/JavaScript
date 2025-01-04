b4aab4500abf3694818ca44b6530c80b47e7dbcf
raidivyanshu19032000 <raidivyanshu19032000@gmail.com>
2025-01-04 13:21:01 +0000

    DataTypes and Variables

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33abc"; 
+
+// console.log(typeof score); 
+// console.log(typeof(score)); 
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+
+let isLoggedIn = 123; 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn); 
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false, "Divyanshu" => true; 
+// 123 => true
+
+
+let someNumber = 123
+
+let stringNumber = String(someNumber); 
+console.log(typeof(stringNumber));
+console.log(stringNumber);
+
