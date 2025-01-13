81f860cd7dd38ae972c645cc0c50d9e27ce34231
raidivyanshu19032000 <raidivyanshu19032000@gmail.com>
2025-01-13 17:29:42 +0000

    object destructing in js

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -54,8 +54,19 @@ users[1].email;
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
 
 
+const course = {
+    courseName : "Js in hindi",
+    price: "999", 
+    courseInstructor : "Divyanshu"
+}
 
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
 
