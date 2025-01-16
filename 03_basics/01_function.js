1736ebda13fd51e244242ce2f3aa2235470a8711
raidivyanshu19032000 <raidivyanshu19032000@gmail.com>
2025-01-16 16:34:21 +0000

    functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -37,12 +37,42 @@ function loginUserMessage(userName){
     return `${userName} just logged in.` 
 }
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
+function calculateCartPrice(val1, val2, ...num1)
+{
+    return num1;
+}
+// console.log(calculateCartPrice(100,200,500,2000));
 
+const user = {
+    userName : "Divyanshu0", 
+    price : 1200
+}
 
+function handleObject(anyObject){
+    console.log(`UserName is ${anyObject.userName} and Price is ${anyObject.prices}`);
+    
+}
+// handleObject(user);
+// we can also pass the direct object in function 
+// handleObject({
+//     userName : "Divyanshu", 
+//     price : 1500
+// });
+
+const myNewArray = [200, 400, 600, 800]; 
+
+function returnSecondValue(getArray){
+    return getArray[1]; 
+}
+// console.log( returnSecondValue(myNewArray)); 
+// console.log(returnSecondValue ([200, 400, 600, 800]));
 
 
 // Point :
 // function
 // Paramater and argument (when we call function that time it is argument and when we define function that time it is called parameter)
+// spread and rest operator (Overview )
+// Pass object to a function 
+//
