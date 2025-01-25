b365ee292bcb31772e8ae1cbb8645059faef5c14
raidivyanshu19032000 <raidivyanshu19032000@gmail.com>
2025-01-25 19:06:31 +0000

    ok

---
+++ 04_control_Flow/one.js
@@ -0,0 +1,57 @@
+// if 
+const userLoggedIn = true;
+const temperature = 41; 
+ 
+if(temperature === 40){
+    // console.log("less than 50");
+}
+else{
+    // console.log("temperature is grater then 40");
+    
+}
+
+// < , > , <=, >=, ==, ===, !=, !==
+
+
+const score = 200; 
+
+if(score >= 200){
+    const power = "fly"
+    // console.log(`Your score is : ${score} and you can: ${power}`);
+}
+// console.log(`Hi, you score is : ${score} and you can : ${power}`);
+
+const balance = 1000; 
+
+// if(balance > 500) console.log(`Your balance  is grater than 500`), console.log(`This is the bad practice to write the coe `);
+
+
+// if(balance < 500){
+//     console.log(`Your balalce is less than 500`);
+// }
+// else if(balance < 750){
+//     console.log(`Your balalce is less than 750`);
+// }
+// else if(balance < 900){
+//     console.log(`Your balalce is less than 900`);
+// }
+// else {
+//     console.log(`Your balalce is grater than 900`);
+// }
+
+// can check multipile condition using and, Or (&, ||)
+// Switch case 
+// Truthy value
+// Nullish Coalescing Operator (??) : null undefined 
+
+let val1; 
+
+// val1 = 5 ?? 10;
+// console.log(val1);
+// val1 = null ?? 10;
+// console.log(val1);
+
+
+// Ternarory operator 
+
+condition ? true : false;
