f013064049f2ab0bd5af7baaca4336063c8081a0
raidivyanshu19032000 <raidivyanshu19032000@gmail.com>
2025-01-30 17:11:36 +0000

    All Dom selectors

---
+++ 06_dom/document.js
@@ -0,0 +1,30 @@
+document.getElementById('title')
+document.getElementById('title').id
+document.getElementById('title').className
+document.getElementById('title').innerHTML
+document.getElementById('title').getAttribute('id')
+document.getElementById('title').setAttribute('class', 'test')
+document.getElementById('title').setAttribute('class', 'test heading')
+
+
+//var const = document.getElementById('title')
+//title.style.backgroundColor = "green"
+//title.style.padding = "15px"
+//title.style.borderRadius = '15px'
+
+// innerText vs textContext (It will all the value which contains hidden value also )
+
+// query selector : 
+// document.querySelector('#title')
+// document.querySelector('input[type="password"]')
+
+
+
+//1.innerHTML: returns all html nodes 
+// 2.textContent : returns all text content
+// 3.innerText : gives the text in the selected node
+// 4.querySelector
+// 5.querySelectorAll
+// 6.getAttribute
+// 7.setAttribute
+
